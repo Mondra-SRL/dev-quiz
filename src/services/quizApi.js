063b5c3f11d3f80003ecf9d7c99263801b234bdd
jaesu968 <mazterk968@gmail.com>
2026-07-10 16:38:54 -0500

    sample quizApi

--- src/services/quizApi.js
+++ src/services/quizApi.js
@@ -1,3 +1,12 @@
+
+
 export async function fetchQuizQuestions() {
-  return [];
+  const res = await fetch("https://quizapi.io/api/v1/questions?quiz_id=cmrfg1u8c0grjfbutigvbsp7f&include_answers=true", {
+    headers: { "X-API-Key": import.meta.env.VITE_QUIZ_API_KEY },
+  });
+  if (!res.ok) {
+    throw new Error(`QuizAPI request failed: ${res.status} ${res.statusText}`);
+  }
+  const questions = await res.json();
+  return questions;
 }
