c34ff2b3e55543735f875142d9899718c472e8b0
jaesu968 <mazterk968@gmail.com>
2026-07-29 10:31:02 -0500

    updated quiz topics to focus on apiQuizIds

--- src/data/quizTopics.js
+++ src/data/quizTopics.js
@@ -11,7 +11,6 @@ const quizTopics = [
     name: 'HTML',
     image: htmlLogo,
     description: 'the standard markup language for structuring web page content using elements and tags.',
-    apiTags: 'html',
     apiQuizId: 'cms3a30pt0lesfbut0jx2p576',
   },
   {
@@ -19,35 +18,35 @@ const quizTopics = [
     name: 'CSS',
     image: cssLogo,
     description: 'a style sheet language used for describing the presentation of HTML documents.',
-    apiCategory: 'css',
+    apiQuizId: 'cmnzv5hc400y32iutw5ywxii1',
   },
   {
     id: 'JavaScript',
     name: 'JavaScript',
     image: jsLogo,
     description: 'a lightweight, interpreted programming language with first-class functions.',
-    apiCategory: 'javascript',
+    apiQuizId: 'cmptrjx7502d07gut7cba4vvg',
   },
   {
     id: 'TypeScript',
     name: 'TypeScript',
     image: tsLogo,
     description: 'a strongly typed programming language that builds on JavaScript.',
-    apiCategory: 'typescript',
+    apiQuizId: 'cmnzuhit0000d2iutbobr9vzj',
   },
   {
     id: 'React',
     name: 'React',
     image: reactLogo,
     description: 'a JavaScript library for building user interfaces using components.',
-    apiCategory: 'react',
+    apiQuizId: 'cmnzuhl2400c52iuti6q4juba'
   },
   {
     id: 'Python',
     name: 'Python',
     image: pythonLogo,
     description: 'a high-level, general-purpose programming language known for its readability.',
-    apiCategory: 'python',
+    apiQuizId: 'cmnzsb1zq001jruuto53x368j'
   },
 ];
 
